e1a0efc5a8ea390abfb6396771bf0c798633e6e2
Richard Tom <tomrichard14@gmail.com>
2018-06-11 13:34:45 -0700

    #1

---
+++ function-drills-2.js
@@ -0,0 +1,8 @@
+function jediName(firstName, lastName) {
+    var last = lastName.slice(0,3);
+    var first = firstName.slice(0,2);
+    console.log(last + first);
+}
+
+jediName('Richard','Tomes');
+
