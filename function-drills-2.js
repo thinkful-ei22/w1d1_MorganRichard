daa07dfff7ccd63a9bcc3b6196d2510b84219db8
MorganHuegel <mhue2910@gmail.com>
2018-06-11 16:44:48 -0400

    To infinity...

--- function-drills-2.js
+++ function-drills-2.js
@@ -6,3 +6,16 @@ function jediName(firstName, lastName) {
 
 jediName('Richard','Tomes');
 
+function beyond(num) {
+    if (num == Infinity) {
+        console.log('And beyond');
+    } else if (num > 0) {
+        console.log('To infinity');
+    } else if (num < 0) {
+        console.log('To negative infinity');
+    } else {
+        console.log('Staying home');
+    }
+}
+
+beyond(-100);
