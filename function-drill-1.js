d8378e0932081c2d217b1381f7018ec3b58b3acb
MorganHuegel <mhue2910@gmail.com>
2018-06-11 16:21:09 -0400

    #8

--- function-drill-1.js
+++ function-drill-1.js
@@ -9,13 +9,16 @@ function whoAmI(name, age) {
     if (!name || !age) {
         throw new Error("Arguement not valid");
     }
+    if (typeof age != 'number') {
+        throw new Error("Age is not a number");
+    }
     var yob = yearOfBirth(age);
     console.log("Hi, my name is " + name + " and I\'m " + age + " years old");
     console.log("I was born in " + yob);
 };
 
 try {
-whoAmI();
+whoAmI('Chris', '29');
 }
 catch(error) {
     console.log(error);
