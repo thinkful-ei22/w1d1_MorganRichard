12fbd844fd3d27764de3cf7086d386fb28b9f856
Richard Tom <tomrichard14@gmail.com>
2018-06-11 16:17:07 -0700

    TA fixes

--- function-drill-1.js
+++ function-drill-1.js
@@ -1,25 +1,26 @@
-function yearOfBirth(age){
-    if (age < 0) {
-        throw new Error("Age can not be negative");
-    }
-    return 2018 - age;
+'use strict';
+function yearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  }
+  return 2018 - age;
 }
 
 function whoAmI(name, age) {
-    if (!name || !age) {
-        throw new Error("Arguement not valid");
-    }
-    if (typeof age != 'number') {
-        throw new Error("Age is not a number");
-    }
-    var yob = yearOfBirth(age);
-    console.log("Hi, my name is " + name + " and I\'m " + age + " years old");
-    console.log("I was born in " + yob);
-};
+  if (!name || !age) {
+    throw new Error('Arguement not valid');
+  }
+  if (typeof age !== 'number') {
+    throw new Error('Age is not a number');
+  }
+  var yob = yearOfBirth(age);
+  console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old');
+  console.log('I was born in ' + yob);
+}
 
 try {
-whoAmI('Chris', '29');
+  whoAmI('Chris', '29');
 }
-catch(error) {
-    console.log(error);
+catch (error) {
+  console.log(error);
 }
--- function-drills-2.js
+++ function-drills-2.js
@@ -1,106 +1,108 @@
+'use strict';
+
 function jediName(firstName, lastName) {
-    var last = lastName.slice(0,3);
-    var first = firstName.slice(0,2);
-    console.log(last + first);
+  var last = lastName.slice(0, 3);
+  var first = firstName.slice(0, 2);
+  console.log(last + first);
 }
 
-jediName('Richard','Tomes');
+jediName('Richard', 'Tomes');
 
 function beyond(num) {
-    if (num == Infinity) {
-        console.log('And beyond');
-    } else if (num > 0) {
-        console.log('To infinity');
-    } else if (num < 0) {
-        console.log('To negative infinity');
-    } else {
-        console.log('Staying home');
-    }
+  if (num === Infinity) {
+    console.log('And beyond');
+  } else if (num > 0) {
+    console.log('To infinity');
+  } else if (num < 0) {
+    console.log('To negative infinity');
+  } else {
+    console.log('Staying home');
+  }
 }
 
 beyond(-100);
 
 function decode(str) {
-    let result = "";
-    let strArray = str.split(" ");
-    const codeObj = {
-        a:2, b:3, c:4, d:5
-    };
-    for (let i=0; i<strArray.length; i++) {
-        if (codeObj.hasOwnProperty(strArray[i][0])) {
-            result += strArray[i][codeObj[strArray[i][0]]-1]
-        }
-        else {
-            result += " ";
-        }     
+  let result = '';
+  let strArray = str.split(' ');
+  const codeObj = {
+    a: 2, b: 3, c: 4, d: 5
+  };
+  for (let i = 0; i < strArray.length; i++) {
+    if (codeObj.hasOwnProperty(strArray[i][0])) {
+      result += strArray[i][codeObj[strArray[i][0]] - 1];
     }
-    console.log(result);
+    else {
+      result += ' ';
+    }
+  }
+  console.log(result);
 }
 
-decode("craft block argon meter bells brown croon droop");
+decode('craft block argon meter bells brown croon droop');
 
-function daysInMonth (month, leapYear=false) {
-    switch(month) {
-        case 'January':
-        case 'May':
-        case 'March':
-        case 'July':
-        case 'August':
-        case 'October':
-        case 'December':
-            console.log(`${month} has 31 days.`); 
-            break;
-        case 'April':
-        case 'June':
-        case 'September':
-        case 'November':
-            console.log(`${month} has 30 days.`); 
-            break;
-        case 'February':
-            if (leapYear) {
-                console.log(`${month} has 29 days`);
-            } else {
-            console.log(`${month} has 28 days`);
-            }
-            break;
-        default: throw new Error('Must provide a valid month.');
+function daysInMonth(month, leapYear = false) {
+  switch (month) {
+  case 'January':
+  case 'May':
+  case 'March':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    console.log(`${month} has 31 days.`);
+    break;
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    console.log(`${month} has 30 days.`);
+    break;
+  case 'February':
+    if (leapYear) {
+      console.log(`${month} has 29 days`);
+    } else {
+      console.log(`${month} has 28 days`);
     }
+    break;
+  default: throw new Error('Must provide a valid month.');
+  }
 }
 try {
-daysInMonth('Fsbruary');
+  daysInMonth('Fsbruary');
 }
 catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
 // const randomNo = Math.floor(Math.random() * 3) + 1;
 function rps(input) {
-    let randomNo = Math.floor(Math.random() * 3) + 1;
-    if (input < 1 || input > 3) {
-        throw new Error("Invalid Input");
-    }
-    if (input == randomNo) {
-        console.log("It is a tie");
-    } else if (input > randomNo) {
-        if (input == 3 && randomNo ==1) {
-            console.log("Lose");
-        }
-        else {
-            console.log("Win");
-        }
+  let randomNo = Math.floor(Math.random() * 3) + 1;
+  if (input < 1 || input > 3) {
+    throw new Error('Invalid Input');
+  }
+  if (input === randomNo) {
+    console.log('It is a tie');
+  } else if (input > randomNo) {
+    if (input === 3 && randomNo === 1) {
+      console.log('Lose');
     }
     else {
-        if (input == 1 && randomNo == 3) {
-            console.log("Win");
-        }
-        else {
-            console.log("Lose");
-        }
+      console.log('Win');
     }
+  }
+  else {
+    if (input === 1 && randomNo === 3) {
+      console.log('Win');
+    }
+    else {
+      console.log('Lose');
+    }
+  }
 }
 try {
-rps(3);
+  rps(3);
 }
 catch (error) {
-    console.log(error);
+  console.log(error);
 }
