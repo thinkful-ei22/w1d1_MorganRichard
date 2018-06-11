c25c20c9e5a5ca9e349bdac3e4b09a60675c25ea
Richard Tom <tomrichard14@gmail.com>
2018-06-11 12:28:21 -0700

    #3

--- function-drill-1.js
+++ function-drill-1.js
@@ -1,6 +1,6 @@
-const whoAmI = function() {
-    console.log('Hi, my name is Chris and I\'m 29 years old');
+function whoAmI(name, age) {
+    console.log("Hi, my name is " + name + " and I\'m " + age + " years old");
 };
 
-whoAmI();
+whoAmI('Chris', 29);
 
