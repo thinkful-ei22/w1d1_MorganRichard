e25d296743302d395180a9705856bbd3df1c06f1
MorganHuegel <mhue2910@gmail.com>
2018-06-11 17:27:48 -0400

    Days in Month

--- function-drills-2.js
+++ function-drills-2.js
@@ -38,3 +38,33 @@ function decode(str) {
 }
 
 decode("craft block argon meter bells brown croon droop");
+
+function daysInMonth (month, leapYear=false) {
+    switch(month) {
+        case 'January':
+        case 'May':
+        case 'March':
+        case 'July':
+        case 'August':
+        case 'October':
+        case 'December':
+            console.log(`${month} has 31 days.`); 
+            break;
+        case 'April':
+        case 'June':
+        case 'September':
+        case 'November':
+            console.log(`${month} has 30 days.`); 
+            break;
+        case 'February':
+            if (leapYear) {
+                console.log(`${month} has 29 days`);
+            } else {
+            console.log(`${month} has 28 days`);
+            }
+            break;
+        default: throw new Error('Must provide a valid month.');
+    }
+}
+
+daysInMonth('february');
