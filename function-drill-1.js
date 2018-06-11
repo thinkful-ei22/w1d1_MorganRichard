8b70c231643b90fa83e15d4c57173ded46630b34
Richard Tom <tomrichard14@gmail.com>
2018-06-11 12:38:15 -0700

    #5

--- function-drill-1.js
+++ function-drill-1.js
@@ -1,7 +1,11 @@
+function yearOfBirth(age){
+    return 2018 - age;
+}
+
 function whoAmI(name, age) {
+    var yob = yearOfBirth(age);
     console.log("Hi, my name is " + name + " and I\'m " + age + " years old");
-    let yearOfBirth = 2018 - age;
-    console.log("I was born in " + yearOfBirth);
+    console.log("I was born in " + yob);
 };
 
 whoAmI('Chris', 29);
