7905f1cd4570fd69a36da97553c06ee541c1c3ff
Richard Tom <tomrichard14@gmail.com>
2018-06-11 12:48:32 -0700

    #6

--- function-drill-1.js
+++ function-drill-1.js
@@ -1,11 +1,19 @@
 function yearOfBirth(age){
+    if (age < 0) {
+        throw new Error("Age can not be negative");
+    }
     return 2018 - age;
 }
 
 function whoAmI(name, age) {
-    var yob = yearOfBirth(age);
+    try {
+        var yob = yearOfBirth(age);
+    }
+    catch(error) {
+        console.error("Argument not valid");
+    }
     console.log("Hi, my name is " + name + " and I\'m " + age + " years old");
     console.log("I was born in " + yob);
 };
 
-whoAmI('Chris', 29);
+whoAmI('Chris', -5);
