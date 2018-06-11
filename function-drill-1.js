08358298587c14de239be7623ee90dd1c8991216
MorganHuegel <mhue2910@gmail.com>
2018-06-11 15:32:04 -0400

    #4

--- function-drill-1.js
+++ function-drill-1.js
@@ -1,6 +1,7 @@
 function whoAmI(name, age) {
     console.log("Hi, my name is " + name + " and I\'m " + age + " years old");
+    let yearOfBirth = 2018 - age;
+    console.log("I was born in " + yearOfBirth);
 };
 
 whoAmI('Chris', 29);
-
